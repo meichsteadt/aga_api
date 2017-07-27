6240577dd8ff4b98bea4f4a5fd8d48d82d847d45
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-07-27 02:33:44 -0700

    remove t.number from first migraiton

--- db/migrate/20170602020701_create_products.rb
+++ db/migrate/20170602020701_create_products.rb
@@ -1,11 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.integer :number
       t.string :name
       t.string :description
       t.string :image
-      
+
       t.timestamps
     end
   end

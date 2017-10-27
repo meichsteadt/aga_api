6150f39d77116023dd3cd9d525bddef9078d96c3
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-10-27 02:35:45 -0700

    try adding perform deliveries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.smtp_settings = {

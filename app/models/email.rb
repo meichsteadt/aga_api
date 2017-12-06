88840ce6a090c3af9f43c2b7ae765ca9de7ed774
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-12-05 19:16:35 -0800

    add dump file

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,13 @@
 class Email < ApplicationRecord
   belongs_to :user
   validates :email_address, :product_id, presence: true
+
+  def self.seed
+    emails = []
+    Email.all.each do |email|
+      new_email = JSON.parse(email.to_json)
+      emails.push(new_email)
+    end
+    emails
+  end
 end

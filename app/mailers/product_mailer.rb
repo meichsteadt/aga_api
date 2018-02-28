ed78268cbf215cb35093f5ea0754255a1f3f3bb8
Matt Eichsteadt <matteichsteadt@gmail.com>
2018-02-28 12:29:31 -0800

    fix email sending stuff

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -2,6 +2,6 @@ class ProductMailer < ApplicationMailer
   def email_product(email_address, product_id)
     @email_address = email_address
     @product = Product.find(product_id)
-    mail(to: @email_address, subject: 'Saved item from kiosk')
+    mail(to: @email_address, subject: 'Homelegance Kiosk Product')
   end
 end

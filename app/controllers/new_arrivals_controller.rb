52d92a3c9e1a05b4e97a7768a6cf247967451340
Matt Eichsteadt <matteichsteadt@gmail.com>
2018-03-12 03:08:32 -0700

    add new arrivals

---
+++ app/controllers/new_arrivals_controller.rb
@@ -0,0 +1,16 @@
+class NewArrivalsController < ApplicationController
+  def index
+    @products = Product.where("created_at >= ?", (DateTime.now - 90))
+    if params[:user_id]
+      sort_by = User.find(params[:user_id]).sort_by
+    end
+    if params[:page_number]
+      pagenumber = params[:page_number].to_i
+      min = (pagenumber - 1) * 6
+      max = min + 5
+      render json: {"products": @products[min..max], "pages": @products.length, "page_number": pagenumber}
+    else
+      render json: @products.sort_by {|e| e.avg_price}.reverse.to_json
+    end
+  end
+end

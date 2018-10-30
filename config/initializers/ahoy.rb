4e67be16469716d6e5b7712cb50d9fd7f2773e1a
Matt Eichsteadt <meichsteadt@Matts-MacBook-Pro.local>
2018-10-30 17:05:42 -0400

    add ahoy

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,12 +1,12 @@
-# class Ahoy::Store < Ahoy::Stores::ActiveRecordTokenStore
-#   # customize here
-#   def user
-#     if @controller.params[:landing_page]
-#       @user = User.find(@controller.params[:landing_page].split('user=').last)
-#     else
-#       @user_id = Visit.find_by_visit_token(@controller.params[:visit_token]).user_id
-#       @user = User.find(@user_id)
-#     end
-#     @user
-#   end
-# end
+class Ahoy::Store < Ahoy::Stores::ActiveRecordTokenStore
+  # customize here
+  def user
+    if @controller.params[:landing_page]
+      @user = User.find(@controller.params[:landing_page].split('user=').last)
+    else
+      @user_id = Visit.find_by_visit_token(@controller.params[:visit_token]).user_id
+      @user = User.find(@user_id)
+    end
+    @user
+  end
+end
